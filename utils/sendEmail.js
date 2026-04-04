ab6bd51501bf15e9623c6aafe7fe36cd5d2573cc
MennaAllahZakaria <mennazakaria2003@gmail.com>
2026-04-04 16:46:45 +0200

    add googleapis dependency and refactor sendEmail function to use Gmail API

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,28 +1,46 @@
-const nodemailer = require("nodemailer");
 
-// Nodemailer
-const sendEmail = async (options) => {
-  // 1) Create transporter ( service that will send email like "gmail","Mailgun", "mialtrap", sendGrid)(object send email)
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT, // if secure false port = 587, if true port= 465
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER, //send by him email
-      pass: process.env.EMAIL_PASSWORD,
-    },
+module.exports = sendEmail;
+const { google } = require("googleapis");
+
+const sendEmail = async ({ Email, subject, message }) => {
+  if (!Email || typeof Email !== "string") {
+    throw new Error("Invalid email address");
+  }
+
+  if (!subject || !message) {
+    throw new Error("Missing subject or message");
+  }
+
+  const oAuth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oAuth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
   });
 
-  // 2) Define email options (like from, to, subject, email content)
-  const mailOpts = {
-    from: "SHEFTAYA <sheftaya.jobs@gmail.com>",
-    to: options.Email,
-    subject: options.subject,
-    text: options.message,
-  };
+  const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
 
-  // 3) Send email
-  await transporter.sendMail(mailOpts);
+  const rawMessage = Buffer.from(
+    `From: SHEFTAYA <sheftaya.jobs@gmail.com>
+To: ${Email}
+Subject: ${subject}
+
+${message}`
+  )
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+
+  await gmail.users.messages.send({
+    userId: "me",
+    requestBody: {
+      raw: rawMessage,
+    },
+  });
 };
 
 module.exports = sendEmail;
