9ad4040a1190caafc739549312da79078038dbfe
MennaAllahZakaria <mennazakaria2003@gmail.com>
2026-07-06 01:19:44 +0300

    feat: integrate sib-api-v3-sdk for email sending functionality

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,28 +1,26 @@
-const nodemailer = require("nodemailer");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
-// Nodemailer
+const client = SibApiV3Sdk.ApiClient.instance;
+
+client.authentications["api-key"].apiKey =
+  process.env.BREVO_API_KEY;
+
+const emailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+
 const sendEmail = async (options) => {
-  // 1) Create transporter ( service that will send email like "gmail","Mailgun", "mialtrap", sendGrid)(object send email)
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT, // if secure false port = 587, if true port= 465
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER, //send by him email
-      pass: process.env.EMAIL_PASSWORD,
+  await emailApi.sendTransacEmail({
+    sender: {
+      name: "SHEFTAYA",
+      email: "sheftaya.jobs@gmail.com",
     },
-  });
-
-  // 2) Define email options (like from, to, subject, email content)
-  const mailOpts = {
-    from: "SHEFTAYA <sheftaya.jobs@gmail.com>",
-    to: options.Email,
+    to: [
+      {
+        email: options.Email,
+      },
+    ],
     subject: options.subject,
-    text: options.message,
-  };
-
-  // 3) Send email
-  await transporter.sendMail(mailOpts);
+    textContent: options.message,
+  });
 };
 
 module.exports = sendEmail;
